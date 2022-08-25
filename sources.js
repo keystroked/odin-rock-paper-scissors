7680627bbaccc1fccd00d249e422c841dff525ab
Austin <austin@misclicks.com>
2022-08-24 21:13:08 -0700

    Fix game function
    Removed return that was exiting the loop

--- sources.js
+++ sources.js
@@ -1,7 +1,4 @@
 const gameOption = ["Rock", "Paper", "Scissors"];
-let draw = "You tied! You both chose " + computerSelection;
-let lose = "You Lose! " + computerSelection + " beats " + playerSelection;
-let win = "You Win! " + playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase() + " beats " + computerSelection;
 
 function getComputerChoice() {
     const randomGameOption = gameOption[Math.floor(Math.random() * gameOption.length)]
@@ -10,19 +7,19 @@ function getComputerChoice() {
 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection.toUpperCase() === computerSelection.toUpperCase()) {
-        return draw;
+        return "You tied! You both chose " + computerSelection;
     }
     else if (playerSelection.toUpperCase() === "ROCK" && computerSelection === "Scissors"){
-        return win;
+        return "You Win! " + playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase() + " beats " + computerSelection;
     }
     else if (playerSelection.toUpperCase() === "SCISSORS" && computerSelection === "Paper"){
-        return win;
+        return "You Win! " + playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase() + " beats " + computerSelection;
     }
     else if (playerSelection.toUpperCase() === "PAPER" && computerSelection === "Scissors"){
-        return win;
+        return "You Win! " + playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase() + " beats " + computerSelection;
     }
     else{
-        return lose;
+        return "You Lose! " + computerSelection + " beats " + playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
     }
 }
 
@@ -30,7 +27,7 @@ function game() {
     for (let i = 0; i < 5; i++) {
         let playerSelection = prompt("Please Choose: Rock | Paper | Scissors... ");
         const computerSelection = getComputerChoice();
-        return console.log(playRound(playerSelection, computerSelection));
+        console.log(playRound(playerSelection, computerSelection));
     }
 }
 
