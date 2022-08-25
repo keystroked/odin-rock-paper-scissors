ce361dee0a78012b785a1fc0ea99a9873ae61272
Austin <austin@misclicks.com>
2022-08-24 21:33:06 -0700

    Complete v1
    v1 of RPS game completed. 5 rounds displayed in the console with score.

--- sources.js
+++ sources.js
@@ -1,4 +1,6 @@
 const gameOption = ["Rock", "Paper", "Scissors"];
+let playerScore = 0;
+let computerScore = 0;
 
 function getComputerChoice() {
     const randomGameOption = gameOption[Math.floor(Math.random() * gameOption.length)]
@@ -10,15 +12,19 @@ function playRound(playerSelection, computerSelection) {
         return "You tied! You both chose " + computerSelection;
     }
     else if (playerSelection.toUpperCase() === "ROCK" && computerSelection === "Scissors"){
+        playerScore = playerScore+1;
         return "You Win! " + playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase() + " beats " + computerSelection;
     }
     else if (playerSelection.toUpperCase() === "SCISSORS" && computerSelection === "Paper"){
+        playerScore = playerScore+1;
         return "You Win! " + playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase() + " beats " + computerSelection;
     }
     else if (playerSelection.toUpperCase() === "PAPER" && computerSelection === "Scissors"){
+        playerScore = playerScore+1;
         return "You Win! " + playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase() + " beats " + computerSelection;
     }
     else{
+        computerScore = computerScore+1;
         return "You Lose! " + computerSelection + " beats " + playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
     }
 }
@@ -28,6 +34,16 @@ function game() {
         let playerSelection = prompt("Please Choose: Rock | Paper | Scissors... ");
         const computerSelection = getComputerChoice();
         console.log(playRound(playerSelection, computerSelection));
+    }
+    if (playerScore > computerScore){
+        console.log("You are the overall winner of this bo5!");
+    }
+    else if (playerScore === computerScore)
+    {
+        console.log("You tied the computer in this bo5");
+    }
+    else {
+        console.log("You lost this bo5! Please try again...");
     }
 }
 
